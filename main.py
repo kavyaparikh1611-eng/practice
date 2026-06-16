c3cd7b33f68da2075269196a0939e2867c0e2729
kavyaparikh1611-eng <kavyaparikh1611@gmail.com>
2026-06-16 08:15:07 +0000

    list \.py

---
+++ main.py
@@ -0,0 +1,21 @@
+a="hello"
+print(a)
+print(type(a))
+b=18
+print(b)
+print(type(b))
+c=45.5
+print(c)
+print(type(c))
+d=1j
+print(d)
+print(type(d))
+e= ["apple", "banana", "cherry"]
+print(e)
+print(type(e))
+f=("apple", "banana", "cherry")
+print(f)
+print(type(f))
+g=range(18)
+print(g)
+print(type(g))
